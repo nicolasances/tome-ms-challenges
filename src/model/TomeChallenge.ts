77f71373d7af5ca9d4bf2c8311e80fbc13a734ef
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-18 10:09:07 +0100

    Generally added Section Index to any challenge

--- src/model/TomeChallenge.ts
+++ src/model/TomeChallenge.ts
@@ -8,6 +8,7 @@ export abstract class TomeChallenge {
     public abstract context: string; 
     public abstract topicId: string;
     public abstract topicCode: string;
+    public abstract sectionIndex: number;
     public abstract sectionCode: string;
     public abstract name: string;
     public abstract description: string;
