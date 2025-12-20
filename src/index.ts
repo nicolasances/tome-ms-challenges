84d9f028335f70fcf14d72b05280ae2d10204031
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-20 17:01:43 +0100

    Get the list of trials

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { PostTrial } from "./dlg/trials/PostTrial";
 import { GetChallenge } from "./dlg/GetChallenge";
 import { GetTrial } from "./dlg/trials/GetTrial";
 import { PostAnswer } from "./dlg/trials/PostAnswer";
+import { GetTrials } from "./dlg/trials/GetTrials";
 
 const api = new TotoAPIController(new ControllerConfig({ apiName: "tome-ms-challenges" }, {defaultHyperscaler: "aws", defaultSecretsManagerLocation: "aws"}), { basePath: '/tomechallenges' });
 
@@ -15,6 +16,7 @@ api.path('GET', '/challenges/:challengeId', new GetChallenge());
 api.path('GET', '/topics/:topicId/challenges', new GetChallenges());
 
 api.path('POST', '/trials', new PostTrial());
+api.path('GET', '/trials', new GetTrials());
 api.path('GET', '/trials/:trialId', new GetTrial());
 api.path('POST', '/trials/:trialId/answers', new PostAnswer());
 
